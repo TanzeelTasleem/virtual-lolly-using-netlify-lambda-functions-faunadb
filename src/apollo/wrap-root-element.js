b30dd2b6615d8a1a53a9f61bc99c71eead9afb08
TanzeelTasleem <tanzeeltasleem648@gmail.com>
2020-11-05 12:28:25 +0500

    feat : wrap root element created and called in gatsby-brower.js and gatsby-ssr.js

---
+++ src/apollo/wrap-root-element.js
@@ -0,0 +1,9 @@
+ 
+import { ApolloProvider } from '@apollo/client'
+import React from  'react'
+import {client} from "./client";
+export const wrapRootElement =({element})=>(
+    <ApolloProvider client={client}>
+        {element}
+    </ApolloProvider>
+)
